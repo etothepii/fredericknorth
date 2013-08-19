f08f62bb5d8f06d3a2608e6732995389fda7befd
jrrpl <jr@epii.co.uk>
2013-08-19 22:45:56 +0100

    Added the necessary code to hopefully allow the downloading of unknown images to the Data folder on the disk and read from there. This will allow for someone with a limited amount of space on their computer (i.e. me and my macbook air) to not have to download images that they are never using, they can simply load them onto a webserver they have ready access to and load them as they need them. Additionally for when working in an offline environment I have created a getDummyImage method which returns a grey ligh grey image with the words, unable to locate and the name of the map.

--- src/main/java/uk/co/epii/conservatives/fredericknorth/maps/OSMapLoaderRegistrar.java
+++ src/main/java/uk/co/epii/conservatives/fredericknorth/maps/OSMapLoaderRegistrar.java
@@ -3,8 +3,10 @@ package uk.co.epii.conservatives.fredericknorth.maps;
 import uk.co.epii.conservatives.fredericknorth.Keys;
 import uk.co.epii.conservatives.fredericknorth.utilities.ApplicationContext;
 
+import java.awt.*;
 import java.io.File;
 import java.util.EnumMap;
+import java.util.Map;
 
 /**
  * User: James Robinson
@@ -14,17 +16,32 @@ import java.util.EnumMap;
 public class OSMapLoaderRegistrar {
 
     private static final String MAP_IMAGE_DIRECTORY_KEY = "MapsDirectory";
+    private static final String RemoteMapsURLKey = "RemoteMapsURL";
     private static final String FileFormatKeyFormat = "FormatFor%sMapFile";
 
+
+    private static final String MapTypeWidthKeyFormat = "%sWidth";
+    private static final String MapTypeHeightKeyFormat = "%sHeight";
+
     public static void registerToContext(ApplicationContext applicationContext) {
+        Map<OSMapType, Dimension> mapDimensions = new EnumMap<OSMapType, Dimension>(OSMapType.class);
+        for (OSMapType osMapType : OSMapType.values()) {
+            int width = Integer.parseInt(
+                    applicationContext.getProperty(String.format(MapTypeWidthKeyFormat, osMapType.getName())));
+            int height = Integer.parseInt(
+                    applicationContext.getProperty(String.format(MapTypeHeightKeyFormat, osMapType.getName())));
+            mapDimensions.put(osMapType, new Dimension(width, height));
+        }
         String dataFolder = applicationContext.getNamedInstance(File.class, Keys.DATA_FOLDER).toString();
         EnumMap<OSMapType, String> mapLocationFormatStrings = new EnumMap<OSMapType, String>(OSMapType.class);
         for(OSMapType osMapType : OSMapType.values()) {
-            mapLocationFormatStrings.put(osMapType, applicationContext.getProperty(String.format(FileFormatKeyFormat, osMapType.getName())));
+            mapLocationFormatStrings.put(osMapType,
+                    applicationContext.getProperty(String.format(FileFormatKeyFormat, osMapType.getName())));
         }
         applicationContext.registerDefaultInstance(OSMapLoader.class,
                 new OSMapLoaderImpl(dataFolder + File.separator +
-                        applicationContext.getProperty(MAP_IMAGE_DIRECTORY_KEY), mapLocationFormatStrings));
+                        applicationContext.getProperty(MAP_IMAGE_DIRECTORY_KEY),
+                        applicationContext.getProperty(RemoteMapsURLKey), mapLocationFormatStrings, mapDimensions));
     }
 
 }
