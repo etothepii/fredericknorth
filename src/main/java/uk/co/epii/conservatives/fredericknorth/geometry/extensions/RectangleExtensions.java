ca69b33c4e6bcfcf3c70475ad59de8c04b1966a9
jrrpl <jr@epii.co.uk>
2013-09-03 20:42:00 +0100

    Added a method to Rectangle Extensions to get the surrounding Rectangles from multiple Rectangles rather than just one and additionally for these to not overlap

--- src/main/java/uk/co/epii/conservatives/fredericknorth/geometry/extensions/RectangleExtensions.java
+++ src/main/java/uk/co/epii/conservatives/fredericknorth/geometry/extensions/RectangleExtensions.java
@@ -1,6 +1,11 @@
 package uk.co.epii.conservatives.fredericknorth.geometry.extensions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.awt.*;
+import java.util.*;
+import java.util.List;
 
 /**
  * User: James Robinson
@@ -8,6 +13,8 @@ import java.awt.*;
  * Time: 17:17
  */
 public class RectangleExtensions {
+
+    private static Logger LOG = LoggerFactory.getLogger(RectangleExtensions.class);
 
     public static Point getCenter(Rectangle rectangle) {
         return new Point(rectangle.x + rectangle.width / 2, rectangle.y + rectangle.height / 2);
@@ -22,5 +29,73 @@ public class RectangleExtensions {
     public static Rectangle grow(Rectangle rectangle, int growBy) {
         return new Rectangle(rectangle.x - growBy, rectangle.y - growBy,
                 rectangle.width + growBy * 2, rectangle.height + growBy * 2);
+    }
+
+    public static List<Rectangle> getSurrounding(Rectangle base, Rectangle fillAround) {
+        List<Rectangle> rectangles = getOverlappingSurrounding(base, fillAround);
+        for (int i = 1; i < rectangles.size(); i++) {
+            Rectangle unchanging = rectangles.get(i);
+            for (int j = 0; j < i; j++) {
+                Rectangle reducing = rectangles.get(j);
+                Rectangle intersection = unchanging.intersection(reducing);
+                if (intersection.width * (long)intersection.height > 0) {
+                    List<Rectangle> surrounding = getOverlappingSurrounding(reducing, intersection);
+                    if (surrounding.size() == 1) {
+                        Rectangle reduced = surrounding.get(0);
+                        reduced = reduced.intersection(reducing);
+                        rectangles.set(j, reduced);
+//                        rectangles.set(j, surrounding.get(0));
+                    }
+                    else {
+                        throw new RuntimeException("Confusion");
+                    }
+                }
+            }
+        }
+        for (int i = rectangles.size() - 1; i >= 0; i--) {
+            Rectangle rectangle = rectangles.get(i);
+            if (rectangle.width * rectangle.height == 0) {
+                rectangles.remove(i);
+            }
+        }
+        return rectangles;
+    }
+
+    private static List<Rectangle> getOverlappingSurrounding(Rectangle base, Rectangle fillAround) {
+        LOG.debug("base: {}", base);
+        LOG.debug("fillAround: {}", fillAround);
+        java.util.List<Rectangle> rectangles = new ArrayList<Rectangle>(4);
+        if (fillAround.x > base.x) {
+            rectangles.add(new Rectangle(base.x, base.y, fillAround.x - base.x, base.height));
+        }
+        if (fillAround.y > base.y) {
+            rectangles.add(new Rectangle(base.x, base.y, base.width, fillAround.y - base.y));
+        }
+        if (base.width + base.x > fillAround.width + fillAround.x) {
+            rectangles.add(new Rectangle(fillAround.x + fillAround.width, base.y,
+                    base.width + base.x - fillAround.width - fillAround.x, base.height));
+        }
+        if (base.height + base.y > fillAround.height + fillAround.y) {
+            rectangles.add(new Rectangle(base.x, fillAround.y + fillAround.height, base.width,
+                    base.height + base.y - fillAround.height - fillAround.y));
+        }
+        return rectangles;
+    }
+
+    public static List<Rectangle> getSurrounding(Rectangle base, Collection<Rectangle> fillAround) {
+        List<Rectangle> surrounding = new ArrayList<Rectangle>(Arrays.asList(base));
+        for (Rectangle rectangle : fillAround) {
+            List<Rectangle> previouslySurrounding = surrounding;
+            surrounding = new ArrayList<Rectangle>(surrounding.size() * 4);
+            for (Rectangle previous : previouslySurrounding) {
+                if (rectangle.intersects(previous)) {
+                    surrounding.addAll(getSurrounding(previous, rectangle));
+                }
+                else {
+                    surrounding.add(previous);
+                }
+            }
+        }
+        return surrounding;
     }
 }
--- src/test/java/uk/co/epii/conservatives/fredericknorth/geometry/extensions/RectangleExtensionsTest.java
+++ src/test/java/uk/co/epii/conservatives/fredericknorth/geometry/extensions/RectangleExtensionsTest.java
@@ -3,8 +3,10 @@ package uk.co.epii.conservatives.fredericknorth.geometry.extensions;
 import org.junit.Test;
 
 import java.awt.*;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * User: James Robinson
@@ -93,5 +95,93 @@ public class RectangleExtensionsTest {
         Rectangle result = RectangleExtensions.getScaleInstance(r, point, scale);
         Rectangle expected = new Rectangle(100, 200, 900, 1200);
         assertEquals(expected, result);
+    }
+
+
+    @Test
+    public void getSurroundingTest1() {
+        Rectangle base = new Rectangle(5, 10, 100, 150);
+        Rectangle fillAround = new Rectangle(10, 20, 30, 40);
+        Set<Rectangle> surrounding =
+                new HashSet<Rectangle>(RectangleExtensions.getSurrounding(
+                        base, fillAround));
+        checkResult(base, fillAround, surrounding);
+    }
+
+    @Test
+    public void getSurroundingTest2() {
+        Rectangle base = new Rectangle(5, 10, 100, 150);
+        Rectangle fillAround = new Rectangle(5, 10, 30, 40);
+        Set<Rectangle> surrounding =
+                new HashSet<Rectangle>(RectangleExtensions.getSurrounding(
+                        base, fillAround));
+        checkResult(base, fillAround, surrounding);
+    }
+
+    @Test
+    public void getSurroundingTest3() {
+        Rectangle base = new Rectangle(5, 10, 100, 150);
+        Rectangle fillAround = new Rectangle(90, 5, 30, 40);
+        Set<Rectangle> surrounding =
+                new HashSet<Rectangle>(RectangleExtensions.getSurrounding(
+                        base, fillAround));
+        checkResult(base, fillAround, surrounding);
+    }
+
+    @Test
+    public void getSurroundingTest4() {
+        Rectangle base = new Rectangle(5, 10, 100, 150);
+        Rectangle fillAround = new Rectangle(0, 90, 150, 80);
+        Set<Rectangle> surrounding =
+                new HashSet<Rectangle>(RectangleExtensions.getSurrounding(
+                        base, fillAround));
+        checkResult(base, fillAround, surrounding);
+    }
+
+    @Test
+    public void getSurroundingTest5() {
+        Rectangle base = new Rectangle(5, 10, 100, 150);
+        Collection<Rectangle> fillAround = buildExpectedList(
+                new Rectangle(30, 20, 10, 80),
+                new Rectangle(60, 20, 10, 80));
+        Set<Rectangle> surrounding =
+                new HashSet<Rectangle>(RectangleExtensions.getSurrounding(
+                        base, fillAround));
+        checkResult(base, fillAround, surrounding);
+    }
+
+    private Set<Rectangle> buildExpectedList(Rectangle... rectangles) {
+        Set<Rectangle> list = new HashSet<Rectangle>(4);
+        for (Rectangle rectangle : rectangles) {
+            list.add(rectangle);
+        }
+        return list;
+    }
+
+    private void checkResult(Rectangle base, Rectangle fillAround, Collection<Rectangle> surrounding) {
+        checkResult(base, Arrays.asList(fillAround), surrounding);
+    }
+
+    private void checkResult(Rectangle base, Collection<Rectangle> fillAround, Collection<Rectangle> surrounding) {
+        long totalArea = 0;
+        ArrayList<Rectangle> all = new ArrayList<Rectangle>(fillAround.size() + surrounding.size());
+        for (Rectangle rectangle : fillAround) {
+            Rectangle intersection = base.intersection(rectangle);
+            totalArea += intersection.width * (long)intersection.height;
+            all.add(rectangle);
+        }
+        for (Rectangle rectangle : surrounding) {
+            assertTrue(base.toString() + " contains " + rectangle.toString(), base.contains(rectangle));
+            totalArea += rectangle.width * (long)rectangle.height;
+            all.add(rectangle);
+        }
+        for (int i = 1; i < all.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                Rectangle a = all.get(i);
+                Rectangle b = all.get(j);
+                assertTrue(a.toString() + " intersects " + b.toString(), !a.intersects(b));
+            }
+        }
+        assertEquals("All sum to base area: ", base.width * (long)base.height, totalArea);
     }
 }
