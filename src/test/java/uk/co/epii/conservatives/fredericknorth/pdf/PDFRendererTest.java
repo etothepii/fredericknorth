646c421b5a5f5208c6fc2a9a73cafc296d2cf4ad
jrrpl <jr@epii.co.uk>
2013-12-16 01:23:58 +0000

    Made a place holder for PDF production
    Greats a simple route, this has 3 Dwelling Groups in it of which 2 share the same name. This will allow for rapid development of the changes required to make route maps more readable following on from the changes.

--- src/test/java/uk/co/epii/conservatives/fredericknorth/pdf/PDFRendererTest.java
+++ src/test/java/uk/co/epii/conservatives/fredericknorth/pdf/PDFRendererTest.java
@@ -2,8 +2,21 @@ package uk.co.epii.conservatives.fredericknorth.pdf;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import uk.co.epii.conservatives.fredericknorth.Keys;
+import uk.co.epii.conservatives.fredericknorth.TestApplicationContext;
+import uk.co.epii.conservatives.fredericknorth.boundaryline.BoundaryLineControllerRegistrar;
+import uk.co.epii.conservatives.fredericknorth.dummydata.TestCouncilWard;
+import uk.co.epii.conservatives.fredericknorth.maps.LocationFactoryRegistrar;
+import uk.co.epii.conservatives.fredericknorth.maps.MapLabelFactoryRegistrar;
+import uk.co.epii.conservatives.fredericknorth.maps.MapViewGeneratorRegistrar;
+import uk.co.epii.conservatives.fredericknorth.maps.OSMapLocatorRegistrar;
+import uk.co.epii.conservatives.fredericknorth.opendata.DwellingProcessorRegistrar;
+import uk.co.epii.conservatives.fredericknorth.opendata.PostcodeDatumFactoryRegistrar;
+import uk.co.epii.conservatives.fredericknorth.utilities.ApplicationContext;
+import uk.co.epii.conservatives.fredericknorth.utilities.NullProgressTracker;
 
 import java.awt.*;
 import java.io.File;
@@ -21,38 +34,30 @@ public class PDFRendererTest {
 
     private static final Logger LOG = Logger.getLogger(PDFRendererTest.class);
 
-    @Ignore
+    @Before
+    public void setUp() {
+        TestCouncilWard.reset();
+        TestCouncilWard.initiateRoutes();
+    }
+
     @Test
     public void createSimpleRouteTest() {
-//        ApplicationContext applicationContext = new TestApplicationContext();
-//        PostcodeDatumFactoryRegistrar.registerToContext(applicationContext);
-//        DwellingProcessorRegistrar.registerToContext(applicationContext, new NullProgressTracker(),
-//                PDFRendererTest.class.getResourceAsStream("/millwallDwellingsSubset.txt"));
-//        OSMapLocatorRegistrar.registerToContext(applicationContext);
-//        PostcodeProcessorRegistrar.registerToContext(applicationContext,
-//                PDFRendererTest.class.getResource("/millwallPostcodeSubset.txt"), 0);
-//        LocationFactoryRegistrar.registerToContext(applicationContext);
-//        MapLabelFactoryRegistrar.registerToContext(applicationContext);
-//        XMLSerializerRegistrar.registerToContext(applicationContext);
-//        Rectangle universe =
-//                applicationContext.getDefaultInstance(Council.class).getUniverse();
-//        universe = new Rectangle(universe.x - 500, universe.y - 500, universe.width + 1000, universe.height + 1000);
-//        applicationContext.registerNamedInstance(Rectangle.class, Keys.UNIVERSE, universe);
-//        OSMapLoaderRegistrar.registerToContext(applicationContext);
-//        MapViewGeneratorRegistrar.registerNamedToContext(applicationContext, Keys.PDF_GENERATOR);
-//        MapImageFactoryRegistrar.registerToContext(applicationContext);
-//        PDFRendererRegistrar.registerToContext(applicationContext);
-//        PDFRenderer pdfRenderer = applicationContext.getDefaultInstance(PDFRenderer.class);
-//        Council council = applicationContext.getDefaultInstance(Council.class);
-//        DwellingProcessor dwellingProcessor = applicationContext.getDefaultInstance(DwellingProcessor.class);
-//        Ward ward = council.getWard("E05000583");
-//        Route route = ward.createRoute("Route 2");
-//        route.getDwellingGroups().add(dwellingProcessor.getDwellingGroup("E14 3AS", "CHAPEL HOUSE STREET, LONDON"));
-//        route.getDwellingGroups().add(dwellingProcessor.getDwellingGroup("E14 3AX", "THERMOPYLAE GATE, LONDON"));
-//        route.getDwellingGroups().add(dwellingProcessor.getDwellingGroup("E14 3AU", "MACQUARIE WAY, LONDON"));
-//        File file = new File(System.getProperty("user.home").concat("/frederickNorth/TestOutput.pdf"));
-//        pdfRenderer.buildRouteGuide(route, file);
-        fail("This test is not yet finished");
+        TestApplicationContext testApplicationContext = new TestApplicationContext();
+        MapLabelFactoryRegistrar.registerToContext(testApplicationContext);
+        MapViewGeneratorRegistrar.registerNamedToContext(testApplicationContext, Keys.PDF_GENERATOR, new NullProgressTracker());
+        BoundaryLineControllerRegistrar.registerToContext(testApplicationContext);
+        LocationFactoryRegistrar.registerToContext(testApplicationContext);
+        PDFRendererRegistrar.registerToContext(testApplicationContext);
+        PDFRenderer pdfRenderer = testApplicationContext.getDefaultInstance(PDFRenderer.class);
+        String simpleRouteOutput = System.getProperty("user.home");
+        simpleRouteOutput +=
+                (simpleRouteOutput.endsWith("/") || simpleRouteOutput.endsWith("\\")) ? "" : "/";
+        simpleRouteOutput += testApplicationContext.getProperty("WorkingDirectory");
+        simpleRouteOutput +=
+                (simpleRouteOutput.endsWith("/") || simpleRouteOutput.endsWith("\\")) ? "" : "/";
+        simpleRouteOutput += "simpleRoute.pdf";
+        pdfRenderer.buildRouteGuide(TestCouncilWard.postalDistrictCW1Routes.getRoutes().iterator().next(),
+                new File(simpleRouteOutput));
     }
 
     @Ignore
