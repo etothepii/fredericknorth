8f0ab9ade8ea91892bb49a4914506907cc4ecdd8
jrrpl <jr@epii.co.uk>
2014-02-23 03:33:02 +0000

    Tidyed imports

--- src/test/java/uk/co/epii/conservatives/fredericknorth/geometry/SquareBoxSifterTest.java
+++ src/test/java/uk/co/epii/conservatives/fredericknorth/geometry/SquareBoxSifterTest.java
@@ -7,10 +7,6 @@ import uk.co.epii.conservatives.fredericknorth.geometry.extensions.PolygonExtens
 import uk.co.epii.conservatives.fredericknorth.geometry.extensions.RectangleExtensions;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 /**
