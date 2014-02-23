841342991f4288027b473cf8873e611ce19883a2
jrrpl <jr@epii.co.uk>
2014-02-23 12:21:49 +0000

    Created Shape Extensions

---
+++ src/main/java/uk/co/epii/conservatives/fredericknorth/geometry/extensions/ShapeExtensions.java
@@ -0,0 +1,29 @@
+package uk.co.epii.conservatives.fredericknorth.geometry.extensions;
+
+import java.awt.*;
+
+/**
+ * User: James Robinson
+ * Date: 23/02/2014
+ * Time: 12:17
+ */
+public class ShapeExtensions {
+
+    public static boolean contains(Shape[] shapes, Point point) {
+        for (Shape shape : shapes) {
+            if (shape.contains(point)) return true;
+        }
+        return false;
+    }
+
+    public static boolean contains(Shape[] shapes, int x, int y) {
+        return contains(shapes, new Point(x, y));
+    }
+
+    public static boolean contains(Shape[] shapes, double x, double y) {
+        for (Shape shape : shapes) {
+            if (shape.contains(x, y)) return true;
+        }
+        return false;
+    }
+}
