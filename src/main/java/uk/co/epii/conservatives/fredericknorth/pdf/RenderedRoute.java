3fd39171bd570fc50b83382431b1b331458d73a4
James Robinson <github@epii.co.uk>
2014-09-14 21:42:05 +0100

    Added zxing to pom for QR codes

---
+++ src/main/java/uk/co/epii/conservatives/fredericknorth/pdf/RenderedRoute.java
@@ -0,0 +1,10 @@
+package uk.co.epii.conservatives.fredericknorth.pdf;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jrrpl
+ * Date: 14/09/2014
+ * Time: 21:39
+ */
+public interface RenderedRoute {
+}
