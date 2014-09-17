ef409d7d1850126b90c3badc92515f8dbfa51b0d
James Robinson <github@epii.co.uk>
2014-09-17 20:56:15 +0100

    Started work on Travelling Salesman Router

---
+++ src/main/java/uk/co/epii/conservatives/fredericknorth/routes/TravellingSalesmanRouter.java
@@ -0,0 +1,16 @@
+package uk.co.epii.conservatives.fredericknorth.routes;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * User: James Robinson
+ * Date: 17/09/2014
+ * Time: 20:53
+ */
+public class TravellingSalesmanRouter extends AbstractRouter {
+  @Override
+  public List<Route> createRoutes(RoutableArea routing, Collection<IndivisbleChunk> chunks, int targetSize) {
+    return null;
+  }
+}
