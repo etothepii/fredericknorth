e506c6a27383fe5e049b6a25c2004d8488894399
James Robinson <github@epii.co.uk>
2014-09-17 21:27:13 +0100

    Removed oversized nodes ahead of TSP

--- src/main/java/uk/co/epii/conservatives/fredericknorth/routes/TravellingSalesmanRouter.java
+++ src/main/java/uk/co/epii/conservatives/fredericknorth/routes/TravellingSalesmanRouter.java
@@ -1,6 +1,9 @@
 package uk.co.epii.conservatives.fredericknorth.routes;
 
-import java.util.Collection;
+import uk.co.epii.spencerperceval.tuple.Duple;
+
+import java.awt.*;
+import java.util.*;
 import java.util.List;
 
 /**
@@ -9,8 +12,46 @@ import java.util.List;
  * Time: 20:53
  */
 public class TravellingSalesmanRouter extends AbstractRouter {
+  
+  Map<Point, Duple<List<IndivisbleChunk>, Double>> colocatedChunks;
+  
   @Override
   public List<Route> createRoutes(RoutableArea routing, Collection<IndivisbleChunk> chunks, int targetSize) {
-    return null;
+    reset(routing);
+    groupByLocation(chunks);
+    removeOverLarge(targetSize);
+    solveTravellingSalesmanProplem(targetSize);
+    return routes;
+  }
+
+  private void solveTravellingSalesmanProplem(int targetSize) {
+  }
+
+  private void removeOverLarge(int targetSize) {
+    for (Point point : new ArrayList<Point>(colocatedChunks.keySet())) {
+      Duple<List<IndivisbleChunk>, Double> duple = colocatedChunks.get(point);
+      if (duple.getSecond() > targetSize) {
+        colocatedChunks.remove(point);
+        createRoute(duple.getFirst());
+      }
+    }
+  }
+
+  @Override
+  protected void reset(RoutableArea routing) {
+    super.reset(routing);
+    colocatedChunks = new HashMap<Point, Duple<List<IndivisbleChunk>, Double>>();
+  }
+
+  private void groupByLocation(Collection<IndivisbleChunk> chunks) {
+    for (IndivisbleChunk chunk : chunks) {
+      Duple<List<IndivisbleChunk>, Double> duple = colocatedChunks.get(chunk.getMedian());
+      if (duple == null) {
+        duple = new Duple<List<IndivisbleChunk>, Double>(new ArrayList<IndivisbleChunk>(), 0d);
+        colocatedChunks.put(chunk.getMedian(), duple);
+      }
+      duple.getFirst().add(chunk);
+      duple.setSecond(duple.getSecond() + chunk.size());
+    }
   }
 }
